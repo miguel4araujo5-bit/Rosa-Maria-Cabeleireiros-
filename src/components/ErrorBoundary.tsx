bca68a4cace3d59316b1c03063ecd3433eac34b1
MiguelAraujo <miguel4araujo5@gmail.com>
2026-02-28 01:54:27 +0000

    Create ErrorBoundary.tsx

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import React, { Component, ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+interface State {
+  hasError: boolean
+}
+
+export default class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true }
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ padding: '2rem', textAlign: 'center' }}>
+          <h1>Estamos a atualizar o site</h1>
+          <p>Por favor tente novamente dentro de momentos.</p>
+        </div>
+      )
+    }
+
+    return this.props.children
+  }
+}
