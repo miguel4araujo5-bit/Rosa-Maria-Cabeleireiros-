747e57b424018a6da8b80837eee1946136eed27f
MiguelAraujo <miguel4araujo5@gmail.com>
2026-03-15 12:32:30 +0000

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'rosa-maria-v1'
+const APP_SHELL = ['/', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png', '/favicon.svg']
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', event => {
+  const { request } = event
+
+  if (request.method !== 'GET') {
+    return
+  }
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request).catch(() => caches.match('/'))
+    )
+    return
+  }
+
+  event.respondWith(
+    fetch(request).catch(() => caches.match(request))
+  )
+})
