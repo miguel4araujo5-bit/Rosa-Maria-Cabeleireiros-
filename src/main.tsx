569132a1ae088caee8602073dd676b15ccc4417b
MiguelAraujo <miguel4araujo5@gmail.com>
2026-03-15 12:35:21 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import App from './App'
 import './index.css'
 import ErrorBoundary from './components/ErrorBoundary'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ErrorBoundary>
