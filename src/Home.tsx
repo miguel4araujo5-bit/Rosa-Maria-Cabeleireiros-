e23c83b7f5266b0068ee39da9865e10860dbaa28
MiguelAraujo <miguel4araujo5@gmail.com>
2026-03-15 10:23:45 +0000

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -57,7 +57,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="px-6 pt-2 md:pt-3 pb-0 -mt-1">
+      <section className="px-6 pt-4 md:pt-5 pb-2 mt-0 bg-brand-paper">
         <div className="max-w-5xl mx-auto flex flex-col items-center gap-2">
           <div className="flex items-center justify-center gap-4 md:gap-5 text-stone-500">
             <div className="hidden sm:block w-12 md:w-16 h-px bg-gradient-to-r from-transparent to-stone-200"></div>
